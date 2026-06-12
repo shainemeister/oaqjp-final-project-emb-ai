d0939c828a08c0feb084442ede5607f34ace34a2
shainemeister <sm.message@pm.me>
2026-06-11 23:10:01 -0400

    added working / fully tested  test_*.py file

---
+++ test_emotion_detection.py
@@ -0,0 +1,29 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+class TestEmotionDetection(unittest.TestCase):
+    def test_emotion_detector(self):
+        """Test required statements and dominant emotions."""
+
+        # Test joy
+        result1 = emotion_detector("I am glad this happened")
+        self.assertEqual(result1['dominant_emotion'], 'joy')
+
+        # Test anger
+        result2 = emotion_detector("I am really mad about this")
+        self.assertEqual(result2['dominant_emotion'], 'anger')
+
+        # Test disgust
+        result3 = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result3['dominant_emotion'], 'disgust')
+
+        # Test sadness
+        result4 = emotion_detector("I am so sad about this")
+        self.assertEqual(result4['dominant_emotion'], 'sadness')
+
+        # Test fear
+        result5 = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result5['dominant_emotion'], 'fear')
+
+if __name__ == '__main__':
+    unittest.main()
